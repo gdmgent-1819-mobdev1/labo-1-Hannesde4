8365cfb762064d714201d2a607ec8fd4ce5d83cc
Hannes De Baere <hannes_de_baere@hotmail.com>
2018-09-30 15:57:26 +0200

    edit
    toch nog even verder gewerkt aan deze opdracht wetende dat ik er geen extra punten mee kan verdienen

--- js/parking.js
+++ js/parking.js
@@ -1,5 +1,44 @@
-function getData() {
-  fetch('https://datatank.stad.gent/4/mobiliteit/bezettingparkingsrealtime.json').then(function(response) {
-  });
-}
-getData();
+
+//XMLHttpRequest  object maken, vervolgens database inladen, en request sturen naar server (ref Javascript en jQuery pg373)
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://datatank.stad.gent/4/mobiliteit/bezettingparkingsrealtime.json', true);
+xhr.send();
+
+//hoofdfunctie die pas werkt als de pagina geladen is
+xhr.onload = function() {
+    if(xhr.status === 200) {
+      //functie oproepen met de juiste data
+      parkingStates();
+    }
+};
+
+function parkingStates(){
+  let parkings = JSON.parse(xhr.responseText);
+  console.log("If I can see this, everything is ok!");
+  for (i = 0; i < 5; i++){
+    let parkFree = parkings[i]['parkingStatus']['availableCapacity'];
+    let parkMax = parkings[i]['parkingStatus']['totalCapacity'];
+   //console.log(parkings[i]);
+   
+   
+   //als er minder dan 20% vrije parking is, dan wordt de achtergrond rood
+    if(parkFree < parkMax/5){
+      //console.log("rood");
+      document.getElementById('container').innerHTML += '<div class="parkings" style="background-color:orange;"> <h3>' + (parkings[i]['name'] + '</h3></div>');
+    
+    
+      //als er minder dan 50% vrije parking is, dan wordt de achtergrond oranje
+    }else if(parkFree < parkMax/2){
+      //console.log("oranje");
+      document.getElementById('container').innerHTML += '<div class="parkings" style="background-color:red;"> <h3>' + (parkings[i]['name'] + '</h3></div>');
+    
+    
+      //als voorgaande if statements niet van toepassing waren, dan wordt de achtergrond kleur groen
+    }else{
+      //console.log("groen");
+      document.getElementById('container').innerHTML += '<div class="parkings" style="background-color:green;"> <h3>' + (parkings[i]['name'] + '</h3></div>');
+    };
+    //console.log(parkFree + " from " + parkMax + " =" + parkFree/parkMax);
+    
+  }
+};
